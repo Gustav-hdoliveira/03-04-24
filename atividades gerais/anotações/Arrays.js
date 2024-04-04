3d9a43e9ba1e4435253f8435e4fbd074f67345eb
Gustav-hdoliveira <162641711+Gustav-hdoliveira@users.noreply.github.com>
2024-04-03 21:52:30 -0300

    Add files via upload

---
+++ atividades gerais/anotações/Arrays.js
@@ -0,0 +1,9 @@
+//ARRAYS; SÃO LISTAS DE ELEMENTOS
+const listadecompras = ['batata', 'nescau', 'requeijão']
+//sua numeração de elementos é contada como o primeiro sendo o 0
+const segundointem = listadecompras[1] //'nescau'
+
+const dograces = ['Pastor Alemão', 'Pastor Belga', 'Boxer', 'Chauchau', 'Bobberman']
+const number = readlineSync.question("Escolha um numero de 0 a 4: ");
+const choisedog = dograces[number]
+console.log('A raça selecionada foi', choisedog,)
